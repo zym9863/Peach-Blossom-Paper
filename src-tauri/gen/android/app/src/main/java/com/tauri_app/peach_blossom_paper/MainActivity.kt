34ef9e75b7e09cbfbea6348eb5162b4ee91b6279
zym <202213093023@cuc.edu.cn>
2025-08-16 01:16:56 +0800

    Add generated Android project for Tauri app
    Introduces a full Android project under src-tauri/gen/android, including Gradle build scripts, configuration files, resources, and Kotlin source for MainActivity. This enables building and running the Tauri app on Android. Also updates the macOS icon file.

---
+++ src-tauri/gen/android/app/src/main/java/com/tauri_app/peach_blossom_paper/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.tauri_app.peach_blossom_paper
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
